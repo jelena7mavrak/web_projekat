4841eacf37cdf275628e4a1c311b12b7e22b3154
DinaPetrusic <39498734+DinaPetrusic@users.noreply.github.com>
2019-05-27 21:19:30 +0200

    Pocetni projekat

---
+++ WebApp/WebApp/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using WebApp.Models;
+
+namespace WebApp.Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Product> Products { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
