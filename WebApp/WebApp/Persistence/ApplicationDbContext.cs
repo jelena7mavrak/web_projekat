fdcfcc87ba2f4da4842a047beda5d4f3bf40554e
jelena7mavrak <jelenamavrak7@gmail.com>
2019-06-03 09:48:20 +0200

    izmjena

--- WebApp/WebApp/Persistence/ApplicationDbContext.cs
+++ WebApp/WebApp/Persistence/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace WebApp.Persistence
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Product> Products { get; set; }
+        //public DbSet<Product> Products { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
